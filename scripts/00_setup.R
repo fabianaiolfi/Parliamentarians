13ef81241cb0e7b79f0265f67cfd30909b35b756
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-07 21:20:47 +0200

    Initiate 00_setup.R

---
+++ scripts/00_setup.R
@@ -0,0 +1,10 @@
+library("tidyverse")
+library("here")
+setwd(here())
+# getwd()
+
+# devtools::install_github("zumbov2/swissparl")
+library("swissparl")
+
+#devtools::install_github("ben-aaron188/rgpt3")
+library(rgpt3)
