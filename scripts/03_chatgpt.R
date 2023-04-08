f92fa931303f38c29fc61cc752d70e5bdc1e2688
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-08 17:17:13 +0200

    Setup sample pipeline to query ChatGPT and merge output with business dataframe.

--- scripts/03_chatgpt.R
+++ scripts/03_chatgpt.R
@@ -17,10 +17,33 @@ gpt3_authenticate("ChatGPT_API_Key.txt")
 # Check if API is working. Should return "test successful".
 gpt3_test_completion()
 
+# Create sample file
+sample_business <- sample_n(business_legislative_period_51, 5)
 
+# Query ChatGPT
+# sample_chatgpt_query <- chatgpt(prompt_role_var = sample_business$role,
+#                                 prompt_content_var = sample_business$chatgpt_query,
+#                                 id_var = sample_business$BusinessShortNumber,
+#                                 param_max_tokens = 100,
+#                                 param_n = 1,
+#                                 param_temperature = 0)
+# save(sample_chatgpt_query, file = "data/sample_chatgpt_query.RData")
+load("data/sample_chatgpt_query.RData")
 
+# Convert ChatGPT output to dataframe (Source: https://stackoverflow.com/a/28630369)
+chatgpt_output <- do.call(
+  rbind,
+  Map(data.frame,
+      BusinessShortNumber = sample_chatgpt_query[[1]][["id"]],
+      content = sample_chatgpt_query[[1]][["chatgpt_content"]]))
 
+# Separate numbered bullet points and spread them over separate columns (Source: ChatGPT)
+chatgpt_output <- chatgpt_output %>%
+  separate(content,
+           into = c("Point_1", "Point_2", "Point_3"),
+           sep = "\\s+(?=(1|2|3)\\.\\s)",
+           remove = TRUE, convert = TRUE, extra = "merge", fill = "right")
 
-
-# save output to local file
-# create backup of local data files!
+# Merge ChatGPT output dataframe together with Business dataframe
+sample_business <- sample_business %>% 
+  left_join(chatgpt_output, by = "BusinessShortNumber")
