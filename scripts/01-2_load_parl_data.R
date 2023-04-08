f721849c2af0c308527875417c13b16af1ab4f11
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-08 10:21:26 +0200

    Split parl_data code into two files. 1. Download data from source, store locally 2. Load locally stored data

---
+++ scripts/01-2_load_parl_data.R
@@ -0,0 +1,7 @@
+# Load Locally Stored Parliamentary Data
+
+# Only Run this script if data has already been downloaded via 01-1_get_parl_data.R
+
+load("data/voting_legislative_period_51.RData")
+load("data/business_legislative_period_51.RData")
+load("data/member_council_legislative_period_51.RData")
