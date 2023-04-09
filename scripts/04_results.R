6c49b04e3df03838b67d189da6e7c814262446a8
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-09 20:36:32 +0200

    Prevent duplicate tables in Shiny app

--- scripts/04_results.R
+++ scripts/04_results.R
@@ -23,33 +23,35 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  output$vote_history <- renderUI({
+  person_vote_history <- reactive({
     if (!is.null(input$selected_person)) {
-      person_vote_history <- voting_legislative_period_51 %>%
+      voting_legislative_period_51 %>%
         filter(PersonNumber == input$selected_person) %>%
-        left_join(member_council_legislative_period_51, by = "PersonNumber") %>%
         inner_join(sample_business, by = "BusinessShortNumber") %>%
-        group_by(DecisionText) %>%
+        left_join(member_council_legislative_period_51, by = "PersonNumber") %>%
+        select(BusinessShortNumber, Point_1, Point_2, Point_3, DecisionText) %>%
         arrange(BusinessShortNumber)
-      
-      output_list <- list()
-      
-      for (group in unique(person_vote_history$DecisionText)) {
-        output_list[[group]] <- DTOutput(group)
-        output[[group]] <- renderDT({
-          votes <- person_vote_history %>%
-            filter(DecisionText == group) %>%
-            select(BusinessShortNumber, Point_1, Point_2, Point_3, DecisionText)
-          
-          datatable(votes)
-        })
-      }
-      
-      return(output_list)
     } else {
       return(NULL)
     }
   })
+  
+  output$vote_history <- renderUI({
+    if (!is.null(input$selected_person)) {
+      DTOutput("vote_history_table")
+    } else {
+      return(NULL)
+    }
+  })
+  
+  output$vote_history_table <- renderDT({
+    if (!is.null(input$selected_person)) {
+      datatable(person_vote_history())
+    }
+  })
 }
+
+
+
 
 shinyApp(ui = ui, server = server)
