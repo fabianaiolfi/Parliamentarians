5b549cb68a525e8907a9b1c1a4b22888af12ab8e
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-09 22:31:27 +0200

    Replace PersonNumber in dropdown selector with the parliamentarian's full name

--- scripts/04_shiny_app.R
+++ scripts/04_shiny_app.R
@@ -4,31 +4,27 @@
 
 # Chat GPT Answer to query "Build Web App with Shiny"
 ui <- fluidPage(
-  titlePanel("Parliamentarian Voting History"),
-  
+  titlePanel("Parliamentarian Vote History"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(
-        "selected_person",
-        "Choose a parliamentarian:",
-        choices = unique(member_council_legislative_period_51$PersonNumber),
-        selected = NULL
-      )
+      selectInput("selected_person", "Select a Parliamentarian:", 
+                  choices = setNames(member_council_legislative_period_51$full_name, member_council_legislative_period_51$full_name),
+                  selected = NULL)
     ),
-    
     mainPanel(
       uiOutput("vote_history")
     )
   )
 )
 
+
 server <- function(input, output) {
   person_vote_history <- reactive({
     if (!is.null(input$selected_person)) {
       voting_legislative_period_51 %>%
-        filter(PersonNumber == input$selected_person) %>%
-        inner_join(sample_business, by = "BusinessShortNumber") %>%
         left_join(member_council_legislative_period_51, by = "PersonNumber") %>%
+        filter(full_name == input$selected_person) %>%
+        inner_join(sample_business, by = "BusinessShortNumber") %>%
         select(BusinessShortNumber, Point_1, Point_2, Point_3, DecisionText) %>%
         distinct(BusinessShortNumber, .keep_all = TRUE) %>%
         arrange(BusinessShortNumber)
