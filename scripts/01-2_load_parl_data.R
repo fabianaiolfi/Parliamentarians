932efa4111b00d4a3a99252e2648000fe5186591
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-12 00:14:34 +0200

    Consequently use here()

--- scripts/01-2_load_parl_data.R
+++ scripts/01-2_load_parl_data.R
@@ -2,6 +2,6 @@
 
 # Only Run this script if data has already been downloaded via 01-1_get_parl_data.R
 
-load("data/voting_legislative_period_51.RData")
-load("data/business_legislative_period_51.RData")
-load("data/member_council_legislative_period_51.RData")
+load(here("data", "voting_legislative_period_51.RData"))
+load(here("data", "business_legislative_period_51.RData"))
+load(here("data", "member_council_legislative_period_51.RData"))
