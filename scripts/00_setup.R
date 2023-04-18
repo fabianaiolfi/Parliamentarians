dd12acdd2308e334192eba772855432919ba1659
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-18 20:19:41 +0200

    Prepare for merge

---
+++ scripts/00_setup.R
@@ -0,0 +1,19 @@
+library("tidyverse")
+library("here")
+setwd(here())
+# getwd()
+
+# devtools::install_github("zumbov2/swissparl")
+library("swissparl")
+
+#devtools::install_github("ben-aaron188/rgpt3")
+library(rgpt3)
+
+#install.packages("shiny")
+library(shiny)
+
+#install.packages("dplyr")
+library(dplyr)
+
+#install.packages("DT")
+library(DT)
---
+++ scripts/02_clean_data.R
@@ -0,0 +1,58 @@
+# Prepare Data for ChatGPT API
+
+
+# Functions ---------------------------------------------------------------
+
+# Remove HTML tags (Source: https://stackoverflow.com/a/17227415)
+strip_html <- function(htmlString) {
+  return(gsub("<.*?>", " ", htmlString))
+}
+
+# Remove text in bold tags because these are already summaries (Source: ChatGPT)
+delete_bold_tags <- function(input_string) {
+  # Define the pattern to match text inside <b> tags
+  pattern <- "<b>[^<>]*</b>"
+  # Replace the pattern with an empty string
+  result_string <- gsub(pattern, "", input_string, perl = TRUE)
+  return(result_string)
+}
+
+
+# Clean Data ---------------------------------------------------------------
+# Remove bold tags
+business_legislative_period_51$InitialSituation_clean <- delete_bold_tags(business_legislative_period_51$InitialSituation)
+
+# Remove all HTML tags
+business_legislative_period_51$InitialSituation_clean <- strip_html(business_legislative_period_51$InitialSituation_clean)
+
+# Remove double whitespaces (Source: ChatGPT)
+business_legislative_period_51$InitialSituation_clean <- gsub(" {2,}", " ", business_legislative_period_51$InitialSituation_clean)
+
+
+
+# Create ChatGPT Query ---------------------------------------------------------------
+summary_query <- "Zusammenfassung in 3 nummerierten Stichpunkten, jeweils maximal 10 Wörter, mit Verweis auf die wichtigsten Akteure in jedem Stichpunkt: "
+
+business_legislative_period_51 <- business_legislative_period_51 %>% 
+  mutate(chatgpt_query = paste(summary_query, InitialSituation_clean, sep = " "))
+
+
+# Truncate Long Query ---------------------------------------------------------------
+# The model gpt-3.5-turbo-0301 sets 4096 as the maximum number of tokens for one query (https://platform.openai.com/docs/guides/chat/introduction)
+
+# Calculate number of tokens per query
+# https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+# English: 1 token ~ 4 characters
+
+# Own calculations based on https://platform.openai.com/tokenizer
+# German: 1 token ~ 2.6 characters
+business_legislative_period_51$chatgpt_query_n_tokens <- nchar(business_legislative_period_51$chatgpt_query) / 2.6
+max(business_legislative_period_51$chatgpt_query_n_tokens) # The longest query has 3357 tokens, leaving 738 tokens for a response
+
+# Wrapper requires `prompt_role_var` to be a column in the dataframe
+business_legislative_period_51$role <- "user"
+
+
+# Tweak Council Member DF for Shiny App ---------------------------------------------------------------
+member_council_legislative_period_51 <- member_council_legislative_period_51 %>% 
+  mutate(full_name = paste(FirstName, LastName))
