88f63405e664fe34696119352c0518bc87791202
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-19 09:10:38 +0200

    Display categories in each card

--- scripts/05_shiny_app.R
+++ scripts/05_shiny_app.R
@@ -1,8 +1,10 @@
 # Merge ChatGPT summaries with voting records of parliamentarians
 
 # Create Shiny App
+# "Offene Aussenpolitik":"Liberale Gesellschaft"
 
 # Chat GPT Answer to query "Build Web App with Shiny"
+# Update the UI function
 ui <- fluidPage(
   titlePanel("Parliamentarian Vote History"),
   sidebarLayout(
@@ -20,14 +22,14 @@ ui <- fluidPage(
       )
     ),
     mainPanel(
-      tableOutput("vote_history")
+      uiOutput("vote_history")
     )
   )
 )
 
-
+# Update the server function
 server <- function(input, output) {
-  output$vote_history <- renderTable({
+  output$vote_history <- renderUI({
     # Filter the voting history by the selected parliamentarian
     filtered_voting_history <- voting_legislative_period_51 %>%
       filter(PersonNumber == input$person_number)
@@ -45,11 +47,36 @@ server <- function(input, output) {
     # Remove duplicate items of business
     result <- distinct(result, BusinessShortNumber, .keep_all = TRUE)
     
-    # Remove the BusinessShortNumber column for display purposes
-    result <- select(result, -BusinessShortNumber)
+    # Create the card HTML template
+    card_template <- '
+      <div class="card" style="margin-bottom: 20px; padding: 10px;">
+        <h4><strong>Title:</strong> {Title}</h4>
+        <p><strong>Summary:</strong> {Summary}</p>
+        <p><strong>Categories:</strong> {Category}</p>
+        <p><strong>Decision:</strong> {DecisionText}</p>
+      </div>'
+    #item[,3:10]
+    # Generate the HTML code for each card
+    cards_html <- lapply(1:nrow(result), function(i) {
+      item <- result[i, ]
+      category_names <- colnames(chatgpt_output_df)[3:10]
+      category_values <- paste0(category_names, ": ", item[,3:10], collapse=", ")
+      
+      htmltools::HTML(
+        glue::glue(
+          card_template,
+          Title = item$Title,
+          Summary = item$Summary,
+          Category = category_values,
+          DecisionText = item$DecisionText
+        )
+      )
+    })
     
-    return(result)
+    # Return the HTML code for the cards as a single column
+    do.call(htmltools::tagList, cards_html)
   })
 }
+
 
 shinyApp(ui = ui, server = server)
