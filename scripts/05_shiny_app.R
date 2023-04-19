14268dcd4c9702dbaa11a85f90cc1b15d4b5677f
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-19 08:48:55 +0200

    Create Shiny app displaying vote history and ChatGPT summaries

--- scripts/05_shiny_app.R
+++ scripts/05_shiny_app.R
@@ -7,44 +7,48 @@ ui <- fluidPage(
   titlePanel("Parliamentarian Vote History"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("selected_person", "Select a Parliamentarian:", 
-                  choices = setNames(member_council_legislative_period_51$full_name, member_council_legislative_period_51$full_name),
-                  selected = NULL)
+      selectInput(
+        "person_number",
+        "Select a Parliamentarian:",
+        choices = setNames(
+          member_council_legislative_period_51$PersonNumber,
+          paste(
+            member_council_legislative_period_51$FirstName,
+            member_council_legislative_period_51$LastName
+          )
+        )
+      )
     ),
     mainPanel(
-      uiOutput("vote_history")
+      tableOutput("vote_history")
     )
   )
 )
 
 
 server <- function(input, output) {
-  person_vote_history <- reactive({
-    if (!is.null(input$selected_person)) {
-      voting_legislative_period_51 %>%
-        left_join(member_council_legislative_period_51, by = "PersonNumber") %>%
-        filter(full_name == input$selected_person) %>%
-        inner_join(sample_business, by = "BusinessShortNumber") %>%
-        select(BusinessShortNumber, Point_1, Point_2, Point_3, DecisionText) %>%
-        distinct(BusinessShortNumber, .keep_all = TRUE) %>%
-        arrange(BusinessShortNumber)
-    } else {
-      return(NULL)
-    }
-  })
-  
-  output$vote_history <- renderUI({
-    if (!is.null(input$selected_person)) {
-      DTOutput("vote_history_table")
-    } else {
-      return(NULL)
-    }
-  })
-  
-  output$vote_history_table <- renderDT({
-    if (!is.null(input$selected_person)) {
-      datatable(person_vote_history())
-    }
+  output$vote_history <- renderTable({
+    # Filter the voting history by the selected parliamentarian
+    filtered_voting_history <- voting_legislative_period_51 %>%
+      filter(PersonNumber == input$person_number)
+    
+    # Join the filtered voting history with business_legislative_period_51 and chatgpt_output_df
+    result <- filtered_voting_history %>%
+      inner_join(business_legislative_period_51, by = "BusinessShortNumber") %>%
+      inner_join(chatgpt_output_df, by = "BusinessShortNumber")
+    
+    # Select relevant columns and rename them
+    result <- result %>%
+      select(BusinessShortNumber, Title, query_central_stmnt, Category = "Offene Aussenpolitik":"Liberale Gesellschaft", DecisionText) %>%
+      rename(Summary = query_central_stmnt)
+    
+    # Remove duplicate items of business
+    result <- distinct(result, BusinessShortNumber, .keep_all = TRUE)
+    
+    # Remove the BusinessShortNumber column for display purposes
+    result <- select(result, -BusinessShortNumber)
+    
+    return(result)
   })
 }
 
