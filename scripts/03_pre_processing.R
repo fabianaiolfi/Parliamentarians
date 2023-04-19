32581dd952308e43e9f37a763929d63b8ca1de53
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-19 16:16:38 +0200

    Update script with correct ChatGPT queries

--- scripts/03_pre_processing.R
+++ scripts/03_pre_processing.R
@@ -29,12 +29,16 @@ business_legislative_period_51$InitialSituation_clean <- strip_html(business_leg
 business_legislative_period_51$InitialSituation_clean <- gsub(" {2,}", " ", business_legislative_period_51$InitialSituation_clean)
 
 
+# Create ChatGPT Query String ---------------------------------------------------------------
 
-# Create ChatGPT Query ---------------------------------------------------------------
-summary_query <- "Zusammenfassung in 3 nummerierten Stichpunkten, jeweils maximal 10 Wörter, mit Verweis auf die wichtigsten Akteure in jedem Stichpunkt: "
+# Setup query as string
+query_central_stmnt <- "Welche zentrale Aussage soll ein:e Wähler:in von diesem Text mitnehmen? Verwende einfache Sprache, maximal 15 Wörter und maximal 1 Satz.\n"
+query_smartspider_precise <- "Du hast 8 Kategorien:\n1. Offene Aussenpolitik\n2. Liberale Wirtschaftspolitik\n3. Restriktive Finanzpolitik\n4. Law & Order\n5. Restriktive Migrationspolitik\n6. Ausgebauter Umweltschutz\n7. Ausgebauter Sozialstaat\n8. Liberale Gesellschaft\nBasierend auf den Text unten gibst du für jede Kategorie einen Wert zwischen 0 und 100. 100 steht für eine starke Zustimmung für die Kategorie, 0 steht für keine Zustimmung. Gebe 'NA' an, wenn eine Kategorie nicht passt. Gib immer an, welcher Wert zu welcher Kategorie gehört. Gib keine weiteren Informationen.\n"
 
+# Merge ChatGPT query string with description text
 business_legislative_period_51 <- business_legislative_period_51 %>% 
-  mutate(chatgpt_query = paste(summary_query, InitialSituation_clean, sep = " "))
+  mutate(chatgpt_query_central_stmnt = paste(query_central_stmnt, InitialSituation_clean, sep = " ")) %>% 
+  mutate(chatgpt_query_smartspider_precise = paste(query_smartspider_precise, InitialSituation_clean, sep = " ")) 
 
-# Wrapper requires `prompt_role_var` to be a column in the dataframe
+# ChatGPT wrapper requires `prompt_role_var` to be its own column in the dataframe
 business_legislative_period_51$role <- "user"
