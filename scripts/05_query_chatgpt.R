bfa00f0e82656273517055da3ca927f07062c799
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-19 17:03:24 +0200

    Tidy up code

--- scripts/05_query_chatgpt.R
+++ scripts/05_query_chatgpt.R
@@ -10,39 +10,17 @@ gpt3_authenticate("ChatGPT_API_Key.txt")
 
 # Query ChatGPT -----------------------------------------------------
 
-# Get and format the current timestamp
-# Helps preventing overwriting files when saving RData locally
+# Get and format the current timestamp to prevent overwriting files when saving RData locally
 timestamp <- Sys.time()
 formatted_timestamp <- format(timestamp, "%Y%m%d_%H%M%S")
 
 # Query
-# chatgpt_output <- chatgpt(prompt_role_var = business_legislative_period_51_long$role,
-#                           prompt_content_var = business_legislative_period_51_long$query,
-#                           id_var = business_legislative_period_51_long$id,
-#                           param_max_tokens = 100,
-#                           param_n = 1,
-#                           param_temperature = 0)
-# 
-# file_name <- paste0("chatgpt_output_", formatted_timestamp, ".RData")
-# save(chatgpt_output, file = here("data", file_name))
-
-
-# Convert Output to dataframe -----------------------------------------------------
-# Support: https://stackoverflow.com/a/28630369
-
-# chatgpt_output <- do.call(
-#   rbind,
-#   Map(data.frame,
-#       BusinessShortNumber = sample_chatgpt_query[[1]][["id"]],
-#       content = sample_chatgpt_query[[1]][["chatgpt_content"]]))
-
-# # Separate numbered bullet points and spread them over separate columns (Source: ChatGPT)
-# chatgpt_output <- chatgpt_output %>%
-#   separate(content,
-#            into = c("Point_1", "Point_2", "Point_3"),
-#            sep = "\\s+(?=(1|2|3)\\.\\s)",
-#            remove = TRUE, convert = TRUE, extra = "merge", fill = "right")
-# 
-# # Merge ChatGPT output dataframe together with Business dataframe
-# business_legislative_period_51 <- business_legislative_period_51 %>% 
-#   left_join(chatgpt_output, by = "BusinessShortNumber")
+chatgpt_output <- chatgpt(prompt_role_var = business_legislative_period_51_long$role,
+                          prompt_content_var = business_legislative_period_51_long$query,
+                          id_var = business_legislative_period_51_long$id,
+                          param_max_tokens = 100,
+                          param_n = 1,
+                          param_temperature = 0)
+ 
+file_name <- paste0("chatgpt_output_", formatted_timestamp, ".RData")
+save(chatgpt_output, file = here("data", file_name))
--- scripts/06_post_processing.R
+++ scripts/06_post_processing.R
@@ -2,58 +2,42 @@
 
 
 # Load ChatGPT Data -------------------------------------------------------
-
-load(here("data", "chatgpt_output_20230411_232430.RData"))
-chatgpt_output_20230411_232430 <- chatgpt_output
+load(here("data", "chatgpt_output_20230419_165256.RData"))
+chatgpt_output_20230419_165256 <- chatgpt_output
 rm(chatgpt_output)
+
 
 # Convert ChatGPT output to dataframe -------------------------------------------------------
 # Source: https://stackoverflow.com/a/28630369
-chatgpt_output_df_20230411_232430 <- do.call(
+chatgpt_output_df <- do.call(
   rbind,
   Map(data.frame,
-      BusinessShortNumber = chatgpt_output_20230411_232430[[1]][["id"]],
-      content = chatgpt_output_20230411_232430[[1]][["chatgpt_content"]]))
-
-# chatgpt_output_df_20230418_170140 <- do.call(
-#   rbind,
-#   Map(data.frame,
-#       BusinessShortNumber = chatgpt_output_20230418_170140[[1]][["id"]],
-#       content = chatgpt_output_20230418_170140[[1]][["chatgpt_content"]]))
-# 
-# chatgpt_output_df_20230418_210755 <- do.call(
-#   rbind,
-#   Map(data.frame,
-#       BusinessShortNumber = chatgpt_output_20230418_210755[[1]][["id"]],
-#       content = chatgpt_output_20230418_210755[[1]][["chatgpt_content"]]))
+      BusinessShortNumber = chatgpt_output_20230419_165256[[1]][["id"]],
+      content = chatgpt_output_20230419_165256[[1]][["chatgpt_content"]]))
 
 # Split business short number and query type
-chatgpt_output_df_20230411_232430 <- chatgpt_output_df_20230411_232430 %>% 
+chatgpt_output_df <- chatgpt_output_df %>% 
   separate_wider_delim(BusinessShortNumber, delim = "-chatgpt_", names = c("BusinessShortNumber", "query_type"))
 
-# chatgpt_output_df_20230418_170140 <- chatgpt_output_df_20230418_170140 %>% 
-#   separate_wider_delim(BusinessShortNumber, delim = "-chatgpt_", names = c("BusinessShortNumber", "query_type"))
-# 
-# chatgpt_output_df_20230418_210755 <- chatgpt_output_df_20230418_210755 %>% 
-#   separate_wider_delim(BusinessShortNumber, delim = "-chatgpt_", names = c("BusinessShortNumber", "query_type"))
-
-# Merge dataframes
-# chatgpt_output_df <- rbind(chatgpt_output_df_20230411_232430,
-#                            chatgpt_output_df_20230418_170140,
-#                            chatgpt_output_df_20230418_210755)
 
 # Clean ChatGPT output dataframe -------------------------------------------------------
-
 # Convert dataframe
 chatgpt_output_df <- chatgpt_output_df %>% 
-  #filter(query_type == "query_smartspider_precise" | query_type == "query_central_stmnt") %>% 
   pivot_wider(names_from = query_type, values_from = content)
 
 chatgpt_output_df <- chatgpt_output_df %>% 
-  separate(query_smartspider_precise, into = c("Offene Aussenpolitik", "Liberale Wirtschaftspolitik", "Restriktive Finanzpolitik", "Law & Order",
-                                               "Restriktive Migrationspolitik", "Ausgebauter Umweltschutz", "Ausgebauter Sozialstaat", "Liberale Gesellschaft"), sep = "\n")
+  separate(query_smartspider_precise,
+           into = c("Offene Aussenpolitik",
+                    "Liberale Wirtschaftspolitik",
+                    "Restriktive Finanzpolitik",
+                    "Law & Order",
+                    "Restriktive Migrationspolitik",
+                    "Ausgebauter Umweltschutz",
+                    "Ausgebauter Sozialstaat",
+                    "Liberale Gesellschaft"),
+           sep = "\n")
 
-# Custom function to replace entire string with NA if it contains "NA"
+# Custom function to replace entire string with NA if it contains "NA" (ChatGPT)
 replace_NA <- function(x) {
   if (is.character(x)) {
     x[grepl("NA", x)] <- NA
