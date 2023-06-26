2c14305a26807371433a9d5272c3bb36bba4bf10
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-06-26 15:44:34 +0200

    Housekeeping

--- scripts/text2vec/fasttext/04_embed.R
+++ scripts/text2vec/fasttext/04_embed.R
@@ -5,6 +5,9 @@
 list_params <- list(command = 'print-sentence-vectors',
                     model = here("models", "cc.de.300.bin")) # https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.de.300.bin.gz
 
+
+# Create Embeddings -------------------------------------------------------------------
+
 # Uncomment to create embeddings
 # for (col in df_colnames) {
 #   res <- fasttext_interface(list_params,
@@ -13,48 +16,7 @@ list_params <- list(command = 'print-sentence-vectors',
 # }
 
 
-### delete later
-# for (col in df_colnames) {
-#   assign(paste0(col, "_vecs"), scan(here("scripts", "text2vec", "fasttext", "vec_files", paste0(col, ".txt")), character(), sep = "\n"))
-# }
-#   #sf_chatgpt_summaries_vecs <- scan(here("soziale_fragen", "vecs", "chatgpt_summaries.txt"), character(), sep = "\n")
-# 
-# df_colnames_vecs <- paste0(df_colnames, "_vecs")
-# 
-# Description_vecs <- as.data.frame(Description_vecs)
-# 
-# for (i in df_colnames_vecs) {
-#   assign(i, as.data.frame(get(i)))
-# }
-# 
-# TagNames_vecs$`get(i)`
-# 
-# for (j in df_colnames_vecs) {
-#   assign(j, stringr::str_split_fixed(get(j), " ", 300))
-# }
-# 
-# 
-#   
-#   #df_vec <- )
-#   
-#   #df_vec <- as.data.frame(df_vec)
-#   #df_vec <- mutate_all(df_vec, function(x) as.numeric(as.character(x)))
-# 
-# 
-# Proceedings_lemma_vecs$`get(i)`
-# 
-# test <- as.data.frame(BusinessTypeName_vecs)
-#   
-# 
-# test_list <- list(chatgpt_summaries, df)
-# 
-# 
-# 
-# sf_Title_vecs <- cbind(soziale_fragen$Title, sf_Title_vecs)
-# #sf_Title_vecs[!!rowSums(is.na(sf_Title_vecs)),] <- NA
-# 
-# # remove first column
-# sf_BusinessTypeName_vecs[1] <- NULL
+# Import Embeddings -------------------------------------------------------------------
 
 read_embeddings <- function(file_path) {
   file_name <- basename(file_path)
@@ -71,6 +33,10 @@ file_paths <- list.files(file_directory, pattern = "\\.txt$", full.names = TRUE)
 #save(all_vecs, file = here("data", "all_vecs.Rda"))
 rm(all_vecs)
 load(here("data", "all_vecs.Rda"))
+
+
+# Clean Embeddings -------------------------------------------------------------------
+# NAs in the text should have a embedding value of 0
 
 # Remove ".txt" from the file_name column
 for (i in seq_along(all_vecs)) {
@@ -93,7 +59,13 @@ for (i in seq_along(all_vecs)) {
 }
 
 
-# Created weighted sum of all features
+# Create weighted sum of all features -------------------------------------------------------------------
+
+
+
+
+
+# old calculation
 combined_df <- sf_BusinessTypeName_vecs*0.1 +
   sf_Description_vecs*0.5 + # *0.8 +
   sf_FirstCouncil1Name_vecs*0.1 +
