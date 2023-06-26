df5054fc9967d4906f3400b1677cd98da43610cf
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-06-26 14:58:11 +0200

    Create fasttext input files

--- scripts/text2vec/fasttext/03_ft_input_files.R
+++ scripts/text2vec/fasttext/03_ft_input_files.R
@@ -1,3 +1,15 @@
 
 
-write.table(soziale_fragen_edit$chatgpt_summaries, sep=",", "soziale_fragen/chatgpt_summaries.txt", col.names = F, row.names = F, quote = F)
+# Create input files -----------------------------------------------------
+
+df_colnames <- colnames(df)
+df_colnames <- df_colnames[-1]
+
+for (col in df_colnames) {
+  write.table(
+    df[[col]],
+    sep = ",",
+    here("scripts", "text2vec", "fasttext", "input_files", paste0(col, ".txt")),
+    col.names = F, row.names = F, quote = F
+  )
+}
