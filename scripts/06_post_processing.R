8b57474fc28241d2628c0042927a04d905dc1347
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-06-27 21:20:48 +0200

    Update script to work with all businesses

--- scripts/06_post_processing.R
+++ scripts/06_post_processing.R
@@ -2,9 +2,9 @@
 
 
 # Load ChatGPT Data -------------------------------------------------------
-load(here("data", "chatgpt_output_20230419_204609.RData"))
-chatgpt_output_20230419_204609 <- chatgpt_output
-rm(chatgpt_output)
+# load(here("data", "chatgpt_output_20230419_204609.RData"))
+# chatgpt_output_20230419_204609 <- chatgpt_output
+# rm(chatgpt_output)
 
 
 # Convert ChatGPT output to dataframe -------------------------------------------------------
@@ -12,8 +12,8 @@ rm(chatgpt_output)
 chatgpt_output_df <- do.call(
   rbind,
   Map(data.frame,
-      BusinessShortNumber = chatgpt_output_20230419_204609[[1]][["id"]],
-      content = chatgpt_output_20230419_204609[[1]][["chatgpt_content"]]))
+      BusinessShortNumber = chatgpt_output[[1]][["id"]],
+      content = chatgpt_output[[1]][["chatgpt_content"]]))
 
 # Split business short number and query type
 chatgpt_output_df <- chatgpt_output_df %>% 
@@ -25,17 +25,17 @@ chatgpt_output_df <- chatgpt_output_df %>%
 chatgpt_output_df <- chatgpt_output_df %>% 
   pivot_wider(names_from = query_type, values_from = content)
 
-chatgpt_output_df <- chatgpt_output_df %>% 
-  separate(query_smartspider_precise,
-           into = c("Offene Aussenpolitik",
-                    "Liberale Wirtschaftspolitik",
-                    "Restriktive Finanzpolitik",
-                    "Law & Order",
-                    "Restriktive Migrationspolitik",
-                    "Ausgebauter Umweltschutz",
-                    "Ausgebauter Sozialstaat",
-                    "Liberale Gesellschaft"),
-           sep = "\n")
+# chatgpt_output_df <- chatgpt_output_df %>% 
+#   separate(query_smartspider_precise,
+#            into = c("Offene Aussenpolitik",
+#                     "Liberale Wirtschaftspolitik",
+#                     "Restriktive Finanzpolitik",
+#                     "Law & Order",
+#                     "Restriktive Migrationspolitik",
+#                     "Ausgebauter Umweltschutz",
+#                     "Ausgebauter Sozialstaat",
+#                     "Liberale Gesellschaft"),
+#            sep = "\n")
 
 # Custom function to replace entire string with NA if it contains "NA" (ChatGPT)
 replace_NA <- function(x) {
@@ -47,15 +47,19 @@ replace_NA <- function(x) {
 
 value_pattern <- "\\b\\d{2}\\b"
 
-chatgpt_output_df <- chatgpt_output_df %>%
-  mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), replace_NA) %>%
-  mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), list(~str_extract(., value_pattern))) %>% 
-  mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), as.integer)
+# chatgpt_output_df <- chatgpt_output_df %>%
+#   mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), replace_NA) %>%
+#   mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), list(~str_extract(., value_pattern))) %>% 
+#   mutate_at(vars("Offene Aussenpolitik":"Liberale Gesellschaft"), as.integer)
 
 # Save files for Shiny App -------------------------------------------------------
-save(business_legislative_period_51, file = here("scripts", "shiny_app", "data", "business_legislative_period_51.RData"))
-save(chatgpt_output_df, file = here("scripts","shiny_app", "data", "chatgpt_output_df.RData"))
-save(member_council_legislative_period_51, file = here("scripts","shiny_app", "data", "member_council_legislative_period_51.RData"))
-save(voting_legislative_period_51, file = here("scripts","shiny_app", "data", "voting_legislative_period_51.RData"))
+# save(business_legislative_period_51, file = here("scripts", "shiny_app", "data", "business_legislative_period_51.RData"))
+# save(chatgpt_output_df, file = here("scripts","shiny_app", "data", "chatgpt_output_df.RData"))
+# save(member_council_legislative_period_51, file = here("scripts","shiny_app", "data", "member_council_legislative_period_51.RData"))
+# save(voting_legislative_period_51, file = here("scripts","shiny_app", "data", "voting_legislative_period_51.RData"))
 
 
+# Merge with original dataframe -------------------------------------------------------
+
+all_businesses <- all_businesses %>% 
+  left_join(chatgpt_output_df, by = "BusinessShortNumber")
