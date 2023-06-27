d5b3a21cb88ad556a80f0d28cb958bbbd23f7529
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-06-27 11:41:09 +0200

    Attempt to create list of dataframes per main tag

--- scripts/text2vec/fasttext/05_clustering.R
+++ scripts/text2vec/fasttext/05_clustering.R
@@ -1,5 +1,7 @@
 
-# Prepare DF
+
+# Prepare DF ---------------------------------------------------------------------
+
 combined_vecs_scaled <- scale(combined_vecs)
 combined_vecs_scaled <- as.data.frame(combined_vecs_scaled)
 combined_vecs_scaled$BusinessShortNumber <- df$BusinessShortNumber
@@ -10,64 +12,57 @@ combined_vecs_scaled_split <- split(combined_vecs_scaled, combined_vecs_scaled$m
 df_main_tag_split <- split(df, df$main_tag)
 
 
-# hierarchical clustering ---------------------------------------------------------------------
+# Hierarchical clustering ---------------------------------------------------------------------
 # https://www.statology.org/hierarchical-clustering-in-r/
 
-#df_hierarchical <- scale(combined_vecs)
-#df_hierarchical <- as.data.frame(lapply(combined_vecs[, -which(names(combined_vecs) == "BusinessShortNumber")], scale))
+for (i in seq_along(combined_vecs_scaled_split)) {
+  if (nrow(combined_vecs_scaled_split[[i]]) < 9) {
+    # Deal with dataframes/tags with few bills
+    combined_vecs_scaled_split[[i]]$cluster <- 0
+  } else {
+    # Compute distance matrix
+    d <- dist(combined_vecs_scaled_split[[i]][,c(1:300)], method = "euclidean")
+    d <- as.dist(d)
+    
+    # Perform hierarchical clustering using Ward's method
+    final_clust <- hclust(d, method = "ward.D2")
+    #final_clust <- hclust(d, method = "complete")
+    
+    # Cut the dendrogram into k clusters
+    groups <- cutree(final_clust, k = 9)
+    
+    # Append cluster labels to original data
+    combined_vecs_scaled_split[[i]] <- cbind(combined_vecs_scaled_split[[i]], cluster = groups)
+    combined_vecs_scaled_split[[i]] <- combined_vecs_scaled_split[[i]] %>% distinct(BusinessShortNumber, .keep_all = T)
+  }
+}
 
 
-#define linkage methods
-# m <- c( "average", "single", "complete", "ward")
-# names(m) <- c( "average", "single", "complete", "ward")
-# 
-# #function to compute agglomerative coefficient
-# ac <- function(x) {
-#   #for (i in seq_along(combined_vecs_scaled_split)) {
-#   agnes(combined_vecs_scaled_split[[2]], method = x)$ac
-#   #}
+# Merge dataframes ---------------------------------------------------------------------
+
+df_clustered <- as.data.frame(NULL)
+
+# for (i in seq_along(combined_vecs_scaled_split)) {
+  temp_df <- df_main_tag_split[[8]]
+  temp_df <- temp_df %>% distinct(BusinessShortNumber, .keep_all = T)
+  
+  # Append cluster labels to original dataframe
+  temp_df <- cbind(temp_df, cluster = combined_vecs_scaled_split[[8]]$cluster)
+  temp_df <- temp_df %>% distinct(BusinessShortNumber, .keep_all = T) # Repeat this?
+  
+  df_clustered <- rbind(df_clustered, temp_df)
 # }
-# 
-# combined_vecs_scaled_split[[1]]
-
-#calculate agglomerative coefficient for each clustering linkage method
-#sapply(m, ac)
-
-#perform hierarchical clustering using Ward's minimum variance
-clust <- agnes(combined_vecs_scaled_split[[3]], method = "ward")
-
-#produce dendrogram
-#pltree(clust, cex = 0.6, hang = -1, main = "Dendrogram") 
-
-#calculate gap statistic for each number of clusters (up to 10 clusters)
-#gap_stat <- clusGap(df_hierarchical, FUN = hcut, nstart = 25, K.max = 15, B = 50)
-
-#produce plot of clusters vs. gap statistic
-#fviz_gap_stat(gap_stat)
 
 
-#compute distance matrix
-d <- dist(combined_vecs_scaled_split[[3]], method = "euclidean")
-d <- as.dist(d)
 
-#perform hierarchical clustering using Ward's method
-final_clust <- hclust(d, method = "ward.D2")
 
-#cut the dendrogram into k clusters
-groups <- cutree(final_clust, k = 5)
+combined_vecs_scaled_split[[3]]$cluster
 
-#europapolitik <- combined_vecs_scaled_split[[3]]
-europapolitik_df <- df_main_tag_split[[3]]
+inspect_df <- df_main_tag_split[[20]]
+inspect_df <- inspect_df %>% distinct(BusinessShortNumber, .keep_all = T)
 
-#append cluster labels to original data
-#final_data <- cbind(europapolitik, cluster = groups)
-europapolitik_df <- cbind(europapolitik_df, cluster = groups)
-europapolitik_df <- europapolitik_df %>% distinct(BusinessShortNumber, .keep_all = T)
-
-#final_data <- final_data %>% 
-  #left_join(europapolitik_df, by = "BusinessShortNumber")# %>% 
-  #select(-c(main_tag, BusinessTypeName, SubmissionCouncilName, FirstCouncil1Name, chatgpt_summaries.x))
-
+# Append cluster labels to original data
+inspect_df <- cbind(inspect_df, cluster = combined_vecs_scaled_split[[20]]$cluster)
 
 
 
