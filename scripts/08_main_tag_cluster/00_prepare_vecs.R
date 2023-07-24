bc45c75d44b00c426c85fa6860593a667d4b5eda
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-07-24 22:59:06 +0200

    Initiate scripts with vector weights from 06_all_features

---
+++ scripts/08_main_tag_cluster/00_prepare_vecs.R
@@ -0,0 +1,74 @@
+
+# Load All Businesses -------------------------------------------------------------------
+
+load(file = here("data", "all_businesses.RData"))
+
+# Import Embeddings -------------------------------------------------------------------
+
+# Load previously saved embeddings
+load(here("data", "all_vecs_230703.Rda"))
+
+# Embeddings from 05_chatgpt_tagging
+chatgpt_tags <- read_delim(here("scripts", "05_chatgpt_tagging", "vec_files", "chatgpt_tags.txt"), delim = " ", col_names = FALSE)
+chatgpt_tags$X301 <- NULL
+
+
+# Clean Embeddings -------------------------------------------------------------------
+
+# Remove ".txt" from the file_name column
+for (i in seq_along(all_vecs)) {
+  all_vecs[[i]]$file_name <- gsub("\\.txt$", "", all_vecs[[i]]$file_name)
+}
+
+# NAs in the text should have a embedding value of 0
+# Replace NAs with 0
+all_vecs <- lapply(all_vecs, function(df) {
+  df[is.na(df)] <- 0
+  return(df)
+})
+
+
+
+# Create weighted sum of all features -------------------------------------------------------------------
+
+# Get DF names
+all_vecs_df_names <- c()
+
+for (i in seq_along(all_vecs)) {
+  current_df <- all_vecs[[i]]
+  all_vecs_df_names <- append(all_vecs_df_names, current_df$file_name[1])
+}
+
+all_vecs_df_names <- as.data.frame(all_vecs_df_names)
+all_vecs_df_names <- all_vecs_df_names %>% rowid_to_column("index")
+
+# Remove first two colums of each dataframe
+# Iterate over each dataframe in the list
+for (i in seq_along(all_vecs)) {
+  # Get the current dataframe
+  current_df <- all_vecs[[i]]
+  
+  # Remove the first column in the current dataframe
+  current_df <- subset(current_df, select = -1)
+  
+  # Update the current dataframe in all_vecs with the modified dataframe
+  all_vecs[[i]] <- current_df
+}
+
+# Sum up weighted embeddings
+combined_vecs <- all_vecs[[1]]*0.1 + # BusinessTypeName
+  all_vecs[[3]]*0.1 + # chatgpt_summaries_lemma
+  all_vecs[[4]]*0.1 + # chatgpt_summaries
+  all_vecs[[5]]*0.1 + # Description
+  all_vecs[[6]]*0.1 + # FirstCouncil1Name
+  all_vecs[[8]]*0.1 + # InitialSituation_lemma
+  all_vecs[[10]]*0.1 + # main_tag
+  all_vecs[[11]]*0.1 + # Proceedings_lemma
+  all_vecs[[13]]*0.1 + # ReasonText_lemma
+  all_vecs[[15]]*0.1 + # ResponsibleDepartmentName
+  all_vecs[[16]]*0.1 + # SubmissionCouncilName
+  all_vecs[[17]]*0.1 + # SubmittedBy
+  all_vecs[[18]]*0.1 + # SubmittedText_lemma
+  all_vecs[[20]]*0.1 + # TagNames
+  all_vecs[[21]]*0.1 + # Title
+  chatgpt_tags
---
+++ scripts/08_main_tag_cluster/01_load_data.R
@@ -0,0 +1,71 @@
+
+# Load all businesses
+load(here("data", "all_businesses_and_summaries.RData"))
+
+# Load vector dataframe
+#load(here("data", "all_vecs_230703.Rda"))
+
+
+# Setup Dataframe, one main tag per line
+df <- all_businesses %>% 
+  select(-c(ID, Language, BusinessType, BusinessTypeAbbreviation, DraftText, DocumentationText, MotionText, FederalCouncilResponseText, FederalCouncilProposal, FederalCouncilProposalDate, FederalCouncilProposalText, BusinessStatus, BusinessStatusText, BusinessStatusDate, ResponsibleDepartment, ResponsibleDepartmentAbbreviation, IsLeadingDepartment, Tags, Category, Modified, SubmissionDate, SubmissionCouncil, SubmissionCouncilAbbreviation, SubmissionSession, SubmissionLegislativePeriod, FirstCouncil1, FirstCouncil1Abbreviation, FirstCouncil2, FirstCouncil2Name, FirstCouncil2Abbreviation)) %>% 
+  mutate(main_tag = TagNames) %>% # Save main tag
+  mutate(main_tag = gsub("\\|.*", "", main_tag)) %>%
+  separate_rows(main_tag, sep = "\\|") %>% # Transform column to have one tag per row
+  mutate(TagNames = gsub("\\|", " ", TagNames))
+
+
+# Create vector of main tags
+main_tags <- df %>% 
+  select(main_tag) %>% distinct(main_tag)
+main_tags <- as.vector(main_tags$main_tag)
+
+
+# Add BusinessShortNumber to vectors
+combined_vecs$BusinessShortNumber <- all_businesses$BusinessShortNumber
+
+
+# Example dataframe with most amount of topics
+finanzwesen <- df %>% dplyr::filter(main_tag == "Finanzwesen")
+
+
+# Create vector of BusinessShortNumbers
+finanzwesen_bsn <- finanzwesen %>% select(BusinessShortNumber)
+finanzwesen_bsn <- as.vector(finanzwesen_bsn$BusinessShortNumber)
+
+
+# Filter for finanzwesen_bsn
+finanzwesen_vectors <- combined_vecs %>% dplyr::filter(BusinessShortNumber %in% finanzwesen_bsn)
+finanzwesen_vectors$BusinessShortNumber <- NULL
+
+
+####### clustering
+
+# Prepare DF ---------------------------------------------------------------------
+
+finanzwesen_vectors_scaled <- scale(finanzwesen_vectors)
+finanzwesen_vectors_scaled <- as.data.frame(finanzwesen_vectors_scaled)
+
+
+# Hierarchical clustering ---------------------------------------------------------------------
+# https://www.statology.org/hierarchical-clustering-in-r/
+
+# Compute distance matrix
+d <- dist(finanzwesen_vectors_scaled[,c(1:300)], method = "euclidean")
+d <- as.dist(d)
+
+# Perform hierarchical clustering using Ward's method
+final_clust <- hclust(d, method = "ward.D2")
+
+# Cut the dendrogram into k clusters
+groups <- cutree(final_clust, k = 24)
+
+# Append cluster labels to df
+finanzwesen <- cbind(finanzwesen, cluster = groups)
+#all_businesses_cluster <- all_businesses
+
+finanzwesen %>% 
+  dplyr::filter(cluster == floor(runif(1, min = 1, max = 21))) %>%
+  # dplyr::filter(cluster == 3) %>%
+  select(BusinessShortNumber, cluster, TagNames, Title, InitialSituation, chatgpt_summaries) %>%
+  View()
