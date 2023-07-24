3534b38c233b0b5e82bf3791e3eb4dbd60e62f7a
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-07-24 10:55:28 +0200

    Housekeeping

--- scripts/chatgpt_tagging/12_output.R
+++
@@ -1,6 +0,0 @@
-
-# Save final data from chatgpt_tagging pipelline
-
-# Store data locally ------------------------------------------------------
-
-save(all_businesses_eval, file = here("scripts", "chatgpt_tagging", "chatgpt_tagging_output", "all_businesses_eval.RData"))
--- scripts/text2vec/fasttext/01_load_data.R
+++
@@ -1,8 +0,0 @@
-# Load Locally Stored Parliamentary Data ---------------------------------------------------------------
-# Only run this script if data has already been downloaded via 01_get_parl_data.R
-
-load(here("data", "all_businesses_and_summaries.RData"))
-#load(here("data", "chatgpt_output_20230419_204609.RData"))
-
-ud_model <- udpipe_load_model(here("models", "german-gsd-ud-2.5-191206.udpipe"))
-custom_stopwords <- scan(here("data", "custom_stopwords.txt"), character(), sep = "\n")
