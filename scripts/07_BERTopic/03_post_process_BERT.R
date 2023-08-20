e322026b1e1e7ce758e1c4d9a2afcdcde66683e0
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-08-20 13:55:01 +0200

    Replace fixed number of topics with variable number of topics

--- scripts/07_BERTopic/03_post_process_BERT.R
+++ scripts/07_BERTopic/03_post_process_BERT.R
@@ -1,6 +1,15 @@
 
 # Prepare Topic Probability Data ---------------------------------------------------------------
 
+# Load Topics
+topics <- read.csv(here("scripts", "07_BERTopic", "BERT_data", "topic_info.csv"), header = T)
+
+# Remove outlier topic (Topic: "-1")
+topics <- topics %>% dplyr::filter(Topic != -1)
+
+# Get number of topics
+topics_count <- nrow(topics)
+
 # Load Topic Probabilities
 topic_probs <- read.csv(here("scripts", "07_BERTopic", "BERT_data", "probs.csv"), header = F)
 
@@ -11,27 +20,27 @@ topic_probs <- tibble::rowid_to_column(topic_probs, "index")
 # Get Topic with highest Probilities ---------------------------------------------------------------
 
 # Calculate difference between 3 highest probabilities
-topic_probs$diff_max_3 <- apply(topic_probs[, paste0("V", 1:140)], 1, function(row) {
+topic_probs$diff_max_3 <- apply(topic_probs[, paste0("V", 1:topics_count)], 1, function(row) {
   sorted_values <- sort(row, decreasing = TRUE)
   return(sorted_values[1] - sorted_values[3])
 })
 
 # Get 1st probability (max)
-topic_probs$prob_1 <- do.call(pmax, topic_probs[, paste0("V", 1:140)])
+topic_probs$prob_1 <- do.call(pmax, topic_probs[, paste0("V", 1:topics_count)])
 
 # Get 2nd highest probability
-topic_probs$prob_2 <- apply(topic_probs[, paste0("V", 1:140)], 1, function(x) x[order(x, decreasing = TRUE)[2]])
+topic_probs$prob_2 <- apply(topic_probs[, paste0("V", 1:topics_count)], 1, function(x) x[order(x, decreasing = TRUE)[2]])
 
 # Get 3rd highest probability
-topic_probs$prob_3 <- apply(topic_probs[, paste0("V", 1:140)], 1, function(x) x[order(x, decreasing = TRUE)[3]])
+topic_probs$prob_3 <- apply(topic_probs[, paste0("V", 1:topics_count)], 1, function(x) x[order(x, decreasing = TRUE)[3]])
 
 # Get column name with highest probability
-topic_probs$max_col <- apply(topic_probs[, paste0("V", 1:140)], 1, function(x) {
+topic_probs$max_col <- apply(topic_probs[, paste0("V", 1:topics_count)], 1, function(x) {
   names(x)[which.max(x)]
 })
 
 # Get column names with top 3 probabilities
-topic_probs$top_3_topics <- apply(topic_probs[, paste0("V", 1:140)], 1, function(row) {
+topic_probs$top_3_topics <- apply(topic_probs[, paste0("V", 1:topics_count)], 1, function(row) {
   sorted_indices <- order(row, decreasing = TRUE)[1:3]
   return(paste(names(row)[sorted_indices], collapse = ","))
 })
