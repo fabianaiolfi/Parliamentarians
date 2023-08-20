f0e8399c2e78788b696d37662f069759708977b1
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-08-20 14:42:17 +0200

    Initiate topic generation script

--- scripts/07_BERTopic/04_topic_name_generation.R
+++ scripts/07_BERTopic/04_topic_name_generation.R
@@ -1,6 +1,14 @@
 
-# Topic Name Generation ---------------------------------------------------------------
+# BERTopic Name Generation with ChatGPT ---------------------------------------------------------------
 
-topic_names <- df_long %>% 
-  dplyr::filter(topic_num == "topic_1") %>% 
-  dplyr::filter(prob_1 == 1)
+chatgpt_topics <- topics %>%
+  select(Topic, Representation) %>% 
+  mutate(Representation = gsub("\\[|\\]|'", "", Representation))
+
+query <- "Generiere einen kurzen, allgemein gehaltenen Titel für diese Gruppe von Begriffen:\n"
+
+# Create new column with query and terms
+chatgpt_topics <- chatgpt_topics %>% mutate(chatgpt_query = paste(query, Representation, sep = ""))
+
+# ChatGPT wrapper requires `prompt_role_var` to be its own column in the dataframe
+chatgpt_topics$role <- "user"
