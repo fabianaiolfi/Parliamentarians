7b511f965059122476bc8df2e33382b1f4457212
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-08-27 20:32:56 +0200

    Replaced by 00_main_run_all.R

--- scripts/07_BERTopic/00_run_all.R
+++
@@ -1,6 +0,0 @@
-rm(list=ls()) # Clear enivronment
-
-source(here("scripts", "07_BERTopic", "01_pre_process_BERT.R"))
-source(here("scripts", "07_BERTopic", "03_post_process_BERT.R"))
-source(here("scripts", "07_BERTopic", "04_topic_name_generation.R"))
-source(here("scripts", "07_BERTopic", "05_evaluation.R"))
