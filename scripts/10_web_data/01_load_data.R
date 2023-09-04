b156e4e9b96766690f52f0dd5771c70215e29d22
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-04 19:44:58 +0200

    Prevent items of business appearing twice

--- scripts/10_web_data/01_load_data.R
+++ scripts/10_web_data/01_load_data.R
@@ -21,11 +21,5 @@ rm(voting_legislative_period_47,
       voting_legislative_period_51)
 
 voting_all_periods <- voting_all_periods %>% 
-  select(PersonNumber, FirstName, LastName, CantonName, DecisionText, BusinessShortNumber)
-
-
-# Export for Web ------------------------------
-
-# Assuming all_businesses_web and voting_all_periods are your dataframes
-write_json(all_businesses_web, here("data", "web", "all_businesses_web.json"))
-write_json(voting_all_periods, here("data", "web", "voting_all_periods.json"))
+  select(PersonNumber, FirstName, LastName, CantonName, DecisionText, BusinessShortNumber) %>% 
+  distinct(PersonNumber, BusinessShortNumber, .keep_all = T)
