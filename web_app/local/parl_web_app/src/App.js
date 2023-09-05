bdbe3ec6a7b179c5a09b083feb45a8c884ec4092
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-05 15:23:49 +0200

    Initiate antd-demo

--- web_app/local/parl_web_app/src/App.js
+++ web_app/local/parl_web_app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';  // Import Ant Design styles
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, React!</h1>
+      <Button type="primary">Ant Design Button</Button>
     </div>
   );
 }
