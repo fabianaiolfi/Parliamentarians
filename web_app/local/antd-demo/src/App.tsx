bbb03f4a50d9cd86162efcea8f1313e1930467c3
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-05 17:35:55 +0200

    Update App.tsx

--- web_app/local/antd-demo/src/App.tsx
+++ web_app/local/antd-demo/src/App.tsx
@@ -1,6 +1,8 @@
 import { Button } from 'antd';
 import React from 'react';
 import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
 
 
 // const App: React.FC = () => (
@@ -9,16 +11,101 @@ import React, { useState } from 'react';
 //   </div>
 // );
 
+async function fetchPersons() {
+  return [
+      { PersonNumber: '1', FirstName: 'John', LastName: 'Doe' },
+      { PersonNumber: '2', FirstName: 'Jane', LastName: 'Doe' },
+      // Add more persons here
+  ];
+}
+
+async function fetchBusinessItems(personNumber) {
+  // Replace this with actual fetching logic later
+  return [
+      { BusinessShortNumber: '1', Summary: 'Business 1' },
+      { BusinessShortNumber: '2', Summary: 'Business 2' },
+      // Add more business items here
+  ];
+}
+
+
+
 function App() {
   
   const [persons, setPersons] = useState([]);  // For storing the list of persons
   const [selectedPerson, setSelectedPerson] = useState(null);  // For storing the selected person
+  const [businessItems, setBusinessItems] = useState([]);  // For storing business items
+  const uniqueTopics = [...new Set(businessItems.map(item => item.chatgpt_topic))];
+
+  const groupedBusinessItems = uniqueTopics.map(topic => {
+    // Filter items for this topic
+    const itemsInTopic = businessItems.filter(item => item.chatgpt_topic === topic);
+
+    // Find unique decisions within this topic
+    const uniqueDecisions = [...new Set(itemsInTopic.map(item => item.DecisionText))];
+
+    return (
+        <div key={topic}>
+            <h2>{topic}</h2>
+            {uniqueDecisions.map(decision => {
+                // Filter items for this decision within this topic
+                const itemsInDecision = itemsInTopic.filter(item => item.DecisionText === decision);
+
+                return (
+                    <div key={decision}>
+                        <h3>{decision}</h3>
+                        {itemsInDecision.map(item => (
+                            <div key={item.BusinessShortNumber}>
+                                {/* Your item display logic here */}
+                                {item.Summary}
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
+        </div>
+    );
+});
+
+
+  useEffect(() => {
+    async function fetchData() {
+        const fetchedPersons = await fetchPersons();
+        setPersons(fetchedPersons);
+    }
+    fetchData();
+}, []);  // Empty dependency array means this useEffect runs once when the component mounts
+
+useEffect(() => {
+  if (selectedPerson !== null) {
+      async function fetchData() {
+          const fetchedItems = await fetchBusinessItems(selectedPerson);
+          setBusinessItems(fetchedItems);
+      }
+      fetchData();
+  }
+}, [selectedPerson]);  // This useEffect runs whenever selectedPerson changes
+
+
 
   return (
     <div className="App">
+        {/* Dropdown code */}
+        <div id="business-container">
+            {groupedBusinessItems}
+        </div>
       
       <h1>Hello, React!</h1>
       {/* <Button type="primary">Ant Design Button</Button> */}
+
+      <div id="business-container">
+    {businessItems.map((item) => (
+        <div key={item.BusinessShortNumber}>
+            {item.Summary}
+        </div>
+    ))}
+</div>
+
 
       {/* Dropdown for selecting a person */}
     <div id="person-selector">
@@ -34,9 +121,17 @@ function App() {
     </div>
 
     {/* Container for displaying business items */}
-    <div id="business-items-container">
+    {/* <div id="business-items-container"> */}
         {/* Business items will be populated dynamically */}
-    </div>
+    {/* </div> */}
+
+    {itemsInDecision.map(item => (
+    <Card key={item.BusinessShortNumber} title={item.BusinessShortNumber}>
+        {/* Your item display logic here */}
+        {item.Summary}
+    </Card>
+))}
+
     
     </div>
   );
