3f9eef2ea6ac825780b049a68ea265927702e813
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-11 20:43:03 +0200

    Display loading spinner on page load

--- vue/antd-demo/src/main.js
+++ vue/antd-demo/src/main.js
@@ -6,3 +6,9 @@ import 'ant-design-vue/dist/reset.css';
 const app = createApp(App);
 
 app.use(Antd).mount('#app');
+
+// Remove the loading element when the app is ready
+const loadingElement = document.getElementById('loading');
+if (loadingElement) {
+  loadingElement.style.display = 'none';
+}
